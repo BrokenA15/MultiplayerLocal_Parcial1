9bb6686432ff50314885e8fe8951225f8d0c8ac4
BrokenA15 <BrokenArrows98@gmail.com>
2026-03-04 20:16:58 -0600

    commit jugador con movimiento

--- Assets/Tony/Scripts/PlayerMovement.cs
+++ Assets/Tony/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 move;
     public float speed = 2;
 
+
     public void OnMove(InputValue action)
     {
         move = action.Get<Vector2>();
     }
  
+
     void Update()
     {
         transform.Translate(move.x * Time.deltaTime * speed, 0, move.y * Time.deltaTime * speed);
