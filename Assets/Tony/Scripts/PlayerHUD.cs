7464242a90370431f00e4110c4d9c42fbcc2e8f2
BrokenA15 <143515116+BrokenA15@users.noreply.github.com>
2026-03-05 14:30:20 -0600

    personajesAnimCaminar

--- Assets/Tony/Scripts/PlayerHUD.cs
+++ Assets/Tony/Scripts/PlayerHUD.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class PlayerHUD : MonoBehaviour
 {
     public TextMeshProUGUI ammoText;
-
+    public GameObject helpText;
     PlayerShoot playerShoot;
 
     public void SetPlayer(GameObject player)
@@ -14,9 +15,17 @@ public class PlayerHUD : MonoBehaviour
 
     void Update()
     {
+        StartCoroutine(FalseActive());
         if (playerShoot != null)
         {
             ammoText.text = "Ammo: " + playerShoot.shootsAvailable;
         }
     }
+
+    private IEnumerator FalseActive()
+    {
+        yield return new WaitForSeconds(6f);
+        helpText.SetActive(false);
+    }
+    
 }
