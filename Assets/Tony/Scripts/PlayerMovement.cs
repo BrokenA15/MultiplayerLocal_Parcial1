8f99894b22920a91d047733e987670b62baa2e10
BrokenA15 <143515116+BrokenA15@users.noreply.github.com>
2026-03-05 01:37:15 -0600

    movJugador

--- Assets/Tony/Scripts/PlayerMovement.cs
+++ Assets/Tony/Scripts/PlayerMovement.cs
@@ -5,13 +5,22 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Vector2 move;
+    private Vector2 look;
     public float speed = 2;
+    public float lookSpeed = 100f;
+    public float rotationSpeed = 10f;
+    public Transform cam;
     
     public void OnMove(InputValue action)
     {
         move = action.Get<Vector2>();
     }
- 
+
+    /*public void OnLook(InputValue action)
+    {
+        look = action.Get<Vector2>();
+    }*/
+    
     public void OnInteract(InputValue value)
     {
         
@@ -21,7 +30,27 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(move.x * Time.deltaTime * speed, 0, move.y * Time.deltaTime * speed);
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+        
+        camForward.y = 0;
+        camRight.y = 0;
+        
+        camForward.Normalize();
+        camRight.Normalize();
+        
+        Vector3 moveDir = camForward * move.y + camRight * move.x;
+        transform.position += moveDir * (speed * Time.deltaTime);
+
+        if (moveDir != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+        if (cam != null)    
+        {
+            cam.RotateAround(transform.position, Vector3.up, look.x * lookSpeed  * Time.deltaTime);
+        }
     }
 }
 
--- Assets/Tony/Scripts/Spawner.cs
+++ Assets/Tony/Scripts/Spawner.cs
@@ -8,25 +8,13 @@ public class Spawner : MonoBehaviour
     public Transform[] spawnpos;
     int spawnCount = 0;
     public Transform ringCenter;
+
     public void OnPlayerJoined(PlayerInput playerInput)
     {
-        StartCoroutine(SpawnRoutine(playerInput));
-    }
-    
-    IEnumerator SpawnRoutine(PlayerInput playerInput)
-    {
-        yield return new WaitForEndOfFrame(); 
-
         Transform spawn = spawnpos[spawnCount];
-
-        Vector3 dir = ringCenter.position - spawn.position;
-        dir.y = 0;
-
         playerInput.transform.SetPositionAndRotation(
             spawn.position,
-            Quaternion.LookRotation(dir)
-        );
-
+            Quaternion.identity);
         spawnCount++;
     }
 }
