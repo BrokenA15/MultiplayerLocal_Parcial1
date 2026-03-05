66bb80ff102e2c09ea8ee64c0b04726f17a800b0
Flextus <benjasg10@gmail.com>
2026-03-04 23:01:14 -0600

    SceneLoader Iniciado, splashscreen listo

---
+++ Assets/SplashScreen/Splashscreen.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Splashscreen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void test()
+    {
+        print("Button Pressed");
+        Debug.Log("Button Pressed");
+    }
+
+}
