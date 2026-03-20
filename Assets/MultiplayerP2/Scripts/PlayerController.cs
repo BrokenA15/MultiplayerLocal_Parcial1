b2816626f0244f1b6819fd6e5b44564e0732cf80
BrokenA15 <BrokenArrows98@gmail.com>
2026-03-19 20:45:29 -0600

    multi misma compu diferentes instancias

--- Assets/MultiplayerP2/Scripts/PlayerController.cs
+++ Assets/MultiplayerP2/Scripts/PlayerController.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
+
+    public TMP_Text textoDelbug;
 
     void Start()
     {
-        if(NetworkManager.Singleton.IsHost)
+        textoDelbug = GameObject.Find("Textito").GetComponent<TMP_Text>();
+        if (NetworkManager.Singleton.IsHost)
         {
             Debug.Log("sos el host");
         }
@@ -18,11 +22,35 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("sos el cliente");
         }
+        ShootClientRPC();
+        ShootServerRPC();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!IsOwner) return;
+
+        if (IsClient)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            float speed = 10 * Time.deltaTime;
+            transform.Translate(new Vector3(x * speed, y * speed));
+
+        }
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    public void ShootClientRPC()
+    {
+        textoDelbug.text = "Shoot Client";
+    }
+
+    [Rpc(SendTo.Server)]
+    public void ShootServerRPC()
+    {
+        textoDelbug.text = "Shoot Server";
+
     }
 }
