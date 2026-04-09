60c73dae9f39dc708da580878347d72884c9ed26
BrokenA15 <143515116+BrokenA15@users.noreply.github.com>
2026-04-08 23:04:16 -0600

    spawns aletaroiso y personajes de prefab

--- Assets/MultiplayerP2/Scripts/ConnectionManager.cs
+++ Assets/MultiplayerP2/Scripts/ConnectionManager.cs
@@ -5,6 +5,14 @@ using Unity.Netcode;
 public class ConnectionManager : MonoBehaviour
 {
 
+    public NetworkManager networkManager;
+    public Transform[] leftSpawnPoints;
+    public Transform[] rightSpawnPoints;
+    
+    public GameObject[] playerPrefabs;
+    
+    private int playerIndex = 0;
+    
     public void Start_Host()
     {
         NetworkManager.Singleton.StartHost();
@@ -26,7 +34,35 @@ public class ConnectionManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        networkManager.OnClientConnectedCallback += OnClientConnected;
+    }
+    
+    private void OnClientConnected(ulong clientId)
+    {
+        if (!NetworkManager.Singleton.IsServer) return;
+
+        GameObject prefabToSpawn = playerPrefabs[playerIndex];
+
+        Transform spawnPoint;
+
+        if (playerIndex == 0)
+        {
+            spawnPoint = leftSpawnPoints[Random.Range(0, leftSpawnPoints.Length)];
+        }
+        else
+        {
+            spawnPoint = rightSpawnPoints[Random.Range(0, rightSpawnPoints.Length)];
+        }
+
+        GameObject player = Instantiate(
+            prefabToSpawn,
+            spawnPoint.position,
+            spawnPoint.rotation
+        );
+
+        player.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId, true);
+
+        playerIndex++;
     }
 
     // Update is called once per frame
