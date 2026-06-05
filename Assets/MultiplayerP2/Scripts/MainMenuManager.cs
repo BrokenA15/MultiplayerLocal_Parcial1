1d96532bf8fab293c1d389382d6605bb702fe2ff
BrokenA15 <143515116+BrokenA15@users.noreply.github.com>
2026-06-04 23:34:44 -0600

    uiiii y mapas

--- Assets/MultiplayerP2/Scripts/MainMenuManager.cs
+++ Assets/MultiplayerP2/Scripts/MainMenuManager.cs
@@ -3,27 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [Header("Configuración de Escenas")]
-    [Tooltip("Nombre exacto de la escena de juego en Build Settings")]
-    [SerializeField] private string nombreEscenaJuego = "P2Multi";
+    [Header("ConfiguraciÃ³n de Escenas")]
+    [SerializeField] private string nombreEscena1 = "P2Multi";
+    [SerializeField] private string nombreEscena2 = "P2Multi2";
+    [SerializeField] private string nombreEscena3 = "P2Multi3";
 
-  
+
     public void Jugar()
     {
-      
-        
-        SceneManager.LoadScene(nombreEscenaJuego);
+        string[] escenas = { nombreEscena1, nombreEscena2, nombreEscena3 };
+        string escenaAleatoria = escenas[Random.Range(0, escenas.Length)];
+        SceneManager.LoadScene(escenaAleatoria);
     }
 
-    
     public void SalirDelJuego()
     {
-        
-
-       
         Application.Quit();
 
-        
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
